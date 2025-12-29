7417f30bee12e84a638ed7e895edff7b1a5fd83e
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2025-12-29 15:40:06 +0800

    Update conftest to ensure determinism

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,10 +12,12 @@ def seed_fixing():
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
     yield
 
 
 def pytest_runtest_setup(item):
-    if "cuda" in [mark.name for mark in item.iter_markers()]:
+    if item.get_closest_marker("cuda"):
         if not torch.cuda.is_available():
             pytest.skip("CUDA not available")
