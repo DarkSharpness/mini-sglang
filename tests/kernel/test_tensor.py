fc0843ad809c0f68139cd62ba20a4173b955f14c
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2025-12-30 03:13:47 +0800

    Refactor test_tensor

--- tests/kernel/test_tensor.py
+++ tests/kernel/test_tensor.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
-from minisgl.kernel import test_tensor
-from minisgl.utils import call_if_main
+import pytest
 import torch
+from minisgl.kernel import test_tensor
 
 
-@call_if_main()
-def main():
-    x = torch.empty((12, 2048), dtype=torch.int32, device="cpu")[:, :1024]
-    y = torch.empty((12, 1024), dtype=torch.int64, device="cuda:1")
-    test_tensor(x, y)
+@pytest.mark.cuda
+def test_test_tensor(cuda_device):
+    x_cpu = torch.randint(0, 100, (12, 2048), dtype=torch.int32, device="cpu")[:, :1024]
+    y_cuda = torch.empty((12, 1024), dtype=torch.int64, device=cuda_device)
+    test_tensor(x_cpu, y_cuda)
+    y_cpu = y_cuda.to("cpu")
+    assert torch.all(y_cpu == x_cpu.long())
