45063c66d7fcb1a9ff2731d2b193497bb5d8c0d3
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2025-12-31 00:14:53 +0800

    Rename import

--- tests/kernel/test_tensor.py
+++ tests/kernel/test_tensor.py
@@ -2,13 +2,13 @@ from __future__ import annotations
 
 import pytest
 import torch
-from minisgl.kernel import test_tensor
+from minisgl.kernel import test_tensor as run_test_tensor
 
 
 @pytest.mark.cuda
-def test_test_tensor(cuda_device):
+def test_test_tensor_correctness(cuda_device):
     x_cpu = torch.randint(0, 100, (12, 2048), dtype=torch.int32, device="cpu")[:, :1024]
     y_cuda = torch.empty((12, 1024), dtype=torch.int64, device=cuda_device)
-    test_tensor(x_cpu, y_cuda)
+    run_test_tensor(x_cpu, y_cuda)
     y_cpu = y_cuda.to("cpu")
     assert torch.all(y_cpu == x_cpu.long())
