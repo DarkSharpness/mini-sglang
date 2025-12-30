50c9d103ea1316c4f73012518092cfd5675d1008
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2025-12-30 22:05:08 +0800

    Group test_backend into classes

--- tests/message/test_backend.py
+++ tests/message/test_backend.py
@@ -4,34 +4,39 @@ from minisgl.core import SamplingParams
 from minisgl.message.backend import BatchBackendMsg, UserMsg, ExitMsg
 
 
-def test_backend_msg_roundtrip():
-    t = torch.tensor([1, 2, 3], dtype=torch.int32)
+class TestUserMsg:
+    def test_roundtrip(self):
+        t = torch.tensor([1, 2, 3], dtype=torch.int32)
+        u_msg = UserMsg(uid=0, input_ids=t, sampling_params=SamplingParams())
+        u_encoded = u_msg.encoder()
+        u_decoded = UserMsg.decoder(u_encoded)
 
-    # Test UserMsg
-    u_msg = UserMsg(uid=0, input_ids=t, sampling_params=SamplingParams())
-    u_encoded = u_msg.encoder()
-    u_decoded = UserMsg.decoder(u_encoded)
+        assert isinstance(u_decoded, UserMsg)
+        assert u_decoded.uid == u_msg.uid
+        assert torch.equal(u_decoded.input_ids, u_msg.input_ids)
+        assert u_decoded.sampling_params == u_msg.sampling_params
 
-    assert isinstance(u_decoded, UserMsg)
-    assert u_decoded.uid == u_msg.uid
-    assert torch.equal(u_decoded.input_ids, u_msg.input_ids)
-    assert u_decoded.sampling_params == u_msg.sampling_params
 
-    # Test BatchBackendMsg
-    b_msg = BatchBackendMsg(data=[u_msg])
-    b_encoded = b_msg.encoder()
-    b_decoded = BatchBackendMsg.decoder(b_encoded)
+class TestBatchBackendMsg:
+    def test_roundtrip(self):
+        t = torch.tensor([1, 2, 3], dtype=torch.int32)
+        u_msg = UserMsg(uid=0, input_ids=t, sampling_params=SamplingParams())
+        b_msg = BatchBackendMsg(data=[u_msg])
+        b_encoded = b_msg.encoder()
+        b_decoded = BatchBackendMsg.decoder(b_encoded)
 
-    assert isinstance(b_decoded, BatchBackendMsg)
-    assert len(b_decoded.data) == 1
-    decoded_inner_msg = b_decoded.data[0]
-    assert isinstance(decoded_inner_msg, UserMsg)
-    assert decoded_inner_msg.uid == u_msg.uid
-    assert torch.equal(decoded_inner_msg.input_ids, u_msg.input_ids)
-    assert decoded_inner_msg.sampling_params == u_msg.sampling_params
+        assert isinstance(b_decoded, BatchBackendMsg)
+        assert len(b_decoded.data) == 1
+        decoded_inner_msg = b_decoded.data[0]
+        assert isinstance(decoded_inner_msg, UserMsg)
+        assert decoded_inner_msg.uid == u_msg.uid
+        assert torch.equal(decoded_inner_msg.input_ids, u_msg.input_ids)
+        assert decoded_inner_msg.sampling_params == u_msg.sampling_params
 
-    # Test ExitMsg
-    e_msg = ExitMsg()
-    e_encoded = e_msg.encoder()
-    e_decoded = ExitMsg.decoder(e_encoded)
-    assert isinstance(e_decoded, ExitMsg)
+
+class TestExitMsg:
+    def test_roundtrip(self):
+        e_msg = ExitMsg()
+        e_encoded = e_msg.encoder()
+        e_decoded = ExitMsg.decoder(e_encoded)
+        assert isinstance(e_decoded, ExitMsg)
