8806de5e201ac0fb5a00c02f013e92381858e7b9
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2025-12-31 00:48:46 +0800

    Remove assertion in test_tensor

--- tests/kernel/test_tensor.py
+++ tests/kernel/test_tensor.py
@@ -10,5 +10,3 @@ def test_test_tensor_ffi_check(cuda_device):
     x_cpu = torch.randint(0, 100, (12, 2048), dtype=torch.int32, device="cpu")[:, :1024]
     y_cuda = torch.empty((12, 1024), dtype=torch.int64, device=cuda_device)
     run_test_tensor(x_cpu, y_cuda)
-    y_cpu = y_cuda.to("cpu")
-    assert torch.all(y_cpu == x_cpu.long())
