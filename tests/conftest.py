ed806c0e08c4549135a08b99ed526881a62e0cee
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2025-12-31 17:17:06 +0800

    Remove distributed fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,29 +26,6 @@ def cuda_device():
     return torch.device("cuda:0")
 
 
-@pytest.fixture(scope="function")
-def distributed_env():
-    """Initializes torch.distributed and returns rank and world size."""
-    if not torch.cuda.is_available() or torch.cuda.device_count() < 2:
-        pytest.skip("distributed tests require at least 2 GPUs")
-
-    tp_rank = int(os.environ["RANK"])
-    tp_size = int(os.environ["WORLD_SIZE"])
-
-    torch.cuda.set_device(tp_rank)
-    torch.cuda.set_stream(torch.cuda.Stream(tp_rank))
-    set_tp_info(tp_rank, tp_size)
-
-    torch.distributed.init_process_group(
-        world_size=tp_size,
-        rank=tp_rank,
-        backend="gloo",
-        init_method="env://",
-    )
-    yield tp_rank, tp_size
-    torch.distributed.destroy_process_group()
-
-
 def pytest_runtest_setup(item):
     # Skip @pytest.mark.cuda if no GPU
     if item.get_closest_marker("cuda"):
